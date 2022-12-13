64aaa727bcd64eb6cc9f88c57b4269104f918bef
KevinA-cpu <kevinanderson1410@gmail.com>
2022-12-13 22:01:50 +0700

    #change doctortime query to id

--- src/doctortime/controller.js
+++ src/doctortime/controller.js
@@ -3,8 +3,10 @@ import queries from "./queries.js";
 
 const getDoctorAvailableShifts = async (req, res) => {
   try {
-    const { name } = req.body;
-    const results = await pool.query(queries.getDoctorAvailableShifts, [name]);
+    const { id_doctortime } = req.body;
+    const results = await pool.query(queries.getDoctorAvailableShifts, [
+      id_doctortime,
+    ]);
     res.status(200).json(results.rows);
   } catch (error) {
     throw error;
@@ -13,15 +15,15 @@ const getDoctorAvailableShifts = async (req, res) => {
 
 const updateDoctorShiftStatus = async (req, res) => {
   try {
-    const { name, shift, action } = req.body;
+    const { id_doctortime, shift, action } = req.body;
     if (action === "book")
-      await pool.query(queries.bookDoctorShift, [name, shift]);
-    else await pool.query(queries.unbookDoctorShift, [name, shift]);
+      await pool.query(queries.bookDoctorShift, [id_doctortime, shift]);
+    else await pool.query(queries.unbookDoctorShift, [id_doctortime, shift]);
     res.status(200).json({
       results: "success",
       message: `shift ${action} successfully`,
       data: {
-        name: name,
+        id_doctortime: id_doctortime,
         shift: shift,
         action: action,
       },
--- src/doctortime/queries.js
+++ src/doctortime/queries.js
@@ -1,9 +1,9 @@
 const getDoctorAvailableShifts =
-  "SELECT dt.shift FROM doctortime dt WHERE doctor = $1 AND status = False;";
+  "SELECT dt.shift FROM doctortime dt WHERE id_doctortime = $1 AND status = False;";
 const bookDoctorShift =
-  "UPDATE doctortime SET status = True WHERE doctor = $1 AND shift = $2;";
+  "UPDATE doctortime SET status = True WHERE id_doctortime = $1 AND shift = $2;";
 const unbookDoctorShift =
-  "UPDATE doctortime SET status = False WHERE doctor = $1 AND shift = $2;";
+  "UPDATE doctortime SET status = False WHERE id_doctortime = $1 AND shift = $2;";
 
 export default {
   getDoctorAvailableShifts,
