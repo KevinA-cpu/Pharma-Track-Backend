f9cad3cf81bff1fb01720b11e349242efb4a03fc
KevinA-cpu <kevinanderson1410@gmail.com>
2022-12-14 07:48:33 +0700

    #change pool to host on render.

--- db.js
+++ db.js
@@ -2,10 +2,10 @@ import pkg from "pg";
 const { Pool } = pkg;
 
 const pool = new Pool({
-  user: "postgres",
-  host: "localhost",
+  user: "pharma_track_user",
+  host: "dpg-cec8smpgp3jg4tel6c90-a",
   database: "pharma_track",
-  password: "root",
+  password: "mOWVNDwa4VoDk2q5mW6OYv76l1Ki1t8P",
   port: 5432,
 });
 
